02d16ba968dfa41495e01241bec1271a9364daf8
Mika'il Khan <13503259+co-category@users.noreply.github.com>
2019-02-08 11:38:19 +0000

    OF-71: remove redundant test

--- test/uk/gov/hmrc/ofstedformsfrontend/controllers/HelloWorldControllerSpec.scala
+++ test/uk/gov/hmrc/ofstedformsfrontend/controllers/HelloWorldControllerSpec.scala
@@ -26,28 +26,28 @@ import play.api.{Configuration, Environment}
 import uk.gov.hmrc.ofstedformsfrontend.config.AppConfig
 
 
-class HelloWorldControllerSpec extends WordSpec with Matchers with GuiceOneAppPerSuite {
-  val fakeRequest = FakeRequest("GET", "/")
-
-  val env = Environment.simple()
-  val configuration = Configuration.load(env)
-
-  val messageApi = new DefaultMessagesApi(env, configuration, new DefaultLangs(configuration))
-  val appConfig = new AppConfig(configuration, env)
-
-  val controller = new HelloWorld(messageApi, appConfig)
-
-  "GET /" should {
-    "return 200" in {
-      val result = controller.helloWorld(fakeRequest)
-      status(result) shouldBe Status.OK
-    }
-
-    "return HTML" in {
-      val result = controller.helloWorld(fakeRequest)
-      contentType(result) shouldBe Some("text/html")
-      charset(result) shouldBe Some("utf-8")
-    }
-
-  }
-}
+//class HelloWorldControllerSpec extends WordSpec with Matchers with GuiceOneAppPerSuite {
+//  val fakeRequest = FakeRequest("GET", "/")
+//
+//  val env = Environment.simple()
+//  val configuration = Configuration.load(env)
+//
+//  val messageApi = new DefaultMessagesApi(env, configuration, new DefaultLangs(configuration))
+//  val appConfig = new AppConfig(configuration, env)
+//
+//  val controller = new HelloWorld(messageApi, appConfig)
+//
+//  "GET /" should {
+//    "return 200" in {
+//      val result = controller.helloWorld(fakeRequest)
+//      status(result) shouldBe Status.OK
+//    }
+//
+//    "return HTML" in {
+//      val result = controller.helloWorld(fakeRequest)
+//      contentType(result) shouldBe Some("text/html")
+//      charset(result) shouldBe Some("utf-8")
+//    }
+//
+//  }
+//}
