c4b9afe0317c323acab5f082206183082f526607
Mika'il Khan <13503259+co-category@users.noreply.github.com>
2019-02-15 10:45:48 +0000

    OF-70: update library & plugin versions

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -8,7 +8,7 @@ object AppDependencies {
   val compile = Seq(
 
     "uk.gov.hmrc"             %% "govuk-template"           % "5.26.0-play-26",
-    "uk.gov.hmrc"             %% "play-ui"                  % "7.27.0-play-26",
+    "uk.gov.hmrc"             %% "play-ui"                  % "7.32.0-play-26",
     "uk.gov.hmrc"             %% "bootstrap-play-26"        % "0.36.0",
     "com.beachape"            %% "enumeratum"               % "1.5.13"
   )
